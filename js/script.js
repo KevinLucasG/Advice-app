48c46f3d3a9b688a9189fe000347c95dc9964fbf
Kevin <kevin.lucassg@gmail.com>
2024-07-09 19:28:46 -0300

    Added Portuguese translation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const adviceText = document.getElementById("advice-text");
 const btnAdvice = document.getElementById("btn-advice");
+const btnLanguage = document.getElementById("btn-language");
 
 let isEnglish = true;
 
-const btnLanguage = document.getElementById("btn-language");
-btnLanguage.addEventListener("click", () => (isEnglish = false));
+btnLanguage.addEventListener("click", () => {
+  isEnglish = !isEnglish; // Inverte o estado de isEnglish
 
-btnAdvice.addEventListener("click", async () => {
+  if (isEnglish) {
+    btnLanguage.innerHTML = "CHANGE TO PORTUGUESE";
+  } else {
+    btnLanguage.innerHTML = "CHANGE TO ENGLISH";
+  }
+
+  // Chama a função para obter e exibir o conselho com o novo idioma
+  fetchAdvice();
+});
+
+btnAdvice.addEventListener("click", fetchAdvice);
+
+async function fetchAdvice() {
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
 
@@ -14,19 +27,40 @@ btnAdvice.addEventListener("click", async () => {
       throw new Error("Erro ao carregar os dados");
     }
 
-    if (isEnglish) {
-      const data = await response.json();
+    const data = await response.json();
 
+    if (isEnglish) {
       adviceText.innerHTML = "";
-      console.log(data);
 
       const newElement = document.createElement("div");
-      newElement.innerHTML = `<p>${data.slip.advice}</p>`;
+      newElement.innerHTML = `<p>“${data.slip.advice}”</p>`;
 
       adviceText.appendChild(newElement);
     } else {
+      const translatedResponse = await fetch(
+        `https://api.mymemory.translated.net/get?q=${data.slip.advice}&langpair=en|pt-br`
+      );
+
+      if (!translatedResponse.ok) {
+        throw new Error("Erro ao carregar a tradução");
+      }
+
+      const translatedData = await translatedResponse.json();
+
+      //Remove double quotes from the beginning and end of the text
+      const translatedText = translatedData.responseData.translatedText.replace(
+        /^"|"$/g,
+        ""
+      );
+
+      adviceText.innerHTML = "";
+
+      const newElement = document.createElement("div");
+      newElement.innerHTML = `<p>“${translatedText}”</p>`;
+
+      adviceText.appendChild(newElement);
     }
   } catch (error) {
     console.error("Erro ao carregar os dados:", error);
   }
-});
+}
